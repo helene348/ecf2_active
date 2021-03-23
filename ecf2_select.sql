41f1368d02437f91fa503373736c8a9d46c456ef
Hélène <helen-r@hotmail.fr>
2021-03-23 18:20:06 +0100

    nouvel ordre des select

---
+++ ecf2_select.sql
@@ -0,0 +1,24 @@
+
+
+select * from "COLLABORATEUR"; -- généré
+select * from "POSTE"; -- généré
+select * from "FRAMEWORK"; --généré
+select * from "IDE"; -- généré
+select * from "LANGAGE"; --généré
+select * from "OS"; --généré
+select * from "SGBD"; -- généré
+select * from "SECTEUR"; --généré
+
+
+select * from "DOCUMENT"; --généré
+select * from "CONTRAT"; --généré
+select * from "OCCUPER"; --généré
+select * from "CLIENT"; -- généré
+select * from "PROJET"; -- généré
+select * from "ETAPE"; -- généré
+select * from "INTERVENIR";-- généré
+select * from "ASSOCIER"; --généré
+select * from "COMPLETER";--généré
+select * from "UTILISER1";
+select * from "UTILISER3";
+select * from "UTILISER2";
